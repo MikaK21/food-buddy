bfb91ccbea5179f88063c5ca609f047e334abaaa
Mika Kessler <mika.kessler213@gmail.com>
2025-01-17 10:29:51 +0100

    Initialize project with Spring Initializr configuration and dependencies

---
+++ src/main/java/com/foodbuddy/food_buddy/FoodBuddyApplication.java
@@ -0,0 +1,15 @@
+package com.foodbuddy.food_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBuddyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FoodBuddyApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/foodbuddy/food_buddy/FoodBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodbuddy.food_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
