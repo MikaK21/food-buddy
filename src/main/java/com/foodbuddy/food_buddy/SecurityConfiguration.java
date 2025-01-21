aaa8123364ad5a55d4db6d1fdeb92a25959f9f55
Mika Kessler <mika.kessler213@gmail.com>
2025-01-21 10:23:39 +0100

    Add in memory user details service to Spring Security

--- src/main/java/com/foodbuddy/food_buddy/SecurityConfiguration.java
+++ src/main/java/com/foodbuddy/food_buddy/SecurityConfiguration.java
@@ -4,6 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,7 +26,29 @@ public class SecurityConfiguration {
                     registry.requestMatchers("/user/**").hasRole("USER");
                     registry.anyRequest().authenticated();
                 })
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                 .build();
     }
 
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails normalUser = User.builder()
+            .username("client")
+            .password("$2a$12$c5xp2b3IC1vYx9IwrP3r5.TRBBvTWjriJ5RWiiNWq5QkMmhY776Yu")
+            .roles("USER")
+            .build();
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password("$2a$12$Ewq7TgaMPbEcduxhxo8VMuS.ptkk7MSNn5m7H75SkpGaPRdoNZ..G")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
